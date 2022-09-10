6180970f0f6d4c51fc886d306cecd523f9c22d29
Sefux <sfuchs@projektkater.de>
2022-09-10 18:12:30 +0200

    Update settings

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -4,9 +4,9 @@ if Rails.env.production? || Rails.env.staging?
 
   Rails.application.config.middleware.use ExceptionNotification::Rack,
     email: {
-      email_prefix:           "[ORTE Backend - #{::Rails.env}] ",
-      sender_address:         %{ dev@a-thousand-channels.xyz  },
-      exception_recipients:   %w{ dev@a-thousand-channels.xyz }
+      email_prefix:           "[LSM ORTE Backend - #{::Rails.env}] ",
+      sender_address:         %{ tech@leerstandsmeldung.de  },
+      exception_recipients:   %w{ tech@leerstandsmeldung.de }
   }
 
 end
