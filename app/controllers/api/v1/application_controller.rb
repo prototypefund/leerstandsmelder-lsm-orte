7e37ee9dc43c8163d34130eae05a4f3b21b65eb1
Sefux <sfuchs@projektkater.de>
2022-10-27 19:19:24 +0200

    Add API response for unauthorized

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -6,4 +6,17 @@ class Api::V1::ApplicationController < ::ApplicationController
 
   protect_from_forgery with: :null_session
   respond_to :json
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def user_not_authorized(exception)
+    # puts 'json: user not authorized'
+    policy_name = exception.policy.class.to_s.underscore
+    respond_to do |format|
+      format.json do
+        self.status = :unauthorized
+        self.response_body = { error: "Access denied: #{policy_name}.#{exception.query}" }.to_json
+      end
+    end
+  end
 end
