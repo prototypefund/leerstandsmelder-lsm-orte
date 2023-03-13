1b0071dcbe7534cf3648431539ccc176ec11d9be
Sefux <sfuchs@projektkater.de>
2023-03-13 15:05:03 +0100

    Fix specs: fix status specs

--- spec/views/status/index.html.haml_spec.rb
+++ spec/views/status/index.html.haml_spec.rb
@@ -4,28 +4,29 @@ require 'rails_helper'
 
 RSpec.describe 'status/index', type: :view do
   before(:each) do
+    @map = FactoryBot.create(:map, title: 'MyMap')
+
     assign(:status, [
              Status.create!(
                title: 'Title',
                description: 'MyText',
                basic: false,
-               map: ''
+               map: @map
              ),
              Status.create!(
                title: 'Title',
                description: 'MyText',
                basic: false,
-               map: ''
+               map: @map
              )
            ])
   end
 
   it 'renders a list of status' do
     render
-    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
+    cell_selector = 'tr>td'
     assert_select cell_selector, text: Regexp.new('Title'.to_s), count: 2
     assert_select cell_selector, text: Regexp.new('MyText'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(false.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(''.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new('MyMap'.to_s), count: 2
   end
 end
--- spec/views/status/new.html.haml_spec.rb
+++ spec/views/status/new.html.haml_spec.rb
@@ -4,25 +4,24 @@ require 'rails_helper'
 
 RSpec.describe 'status/new', type: :view do
   before(:each) do
+    @map = FactoryBot.create(:map)
     assign(:status, Status.new(
                       title: 'MyString',
                       description: 'MyText',
                       basic: false,
-                      map: ''
+                      map: @map
                     ))
   end
 
   it 'renders new status form' do
     render
 
-    assert_select 'form[action=?][method=?]', status_path, 'post' do
+    assert_select 'form[action=?][method=?]', '/status', 'post' do
       assert_select 'input[name=?]', 'status[title]'
 
       assert_select 'textarea[name=?]', 'status[description]'
 
       assert_select 'input[name=?]', 'status[basic]'
-
-      assert_select 'input[name=?]', 'status[map]'
     end
   end
 end
